e320da5f683ef30faa9b35582d6383ff3df81c62
codingcat08 <99567920+codingcat08@users.noreply.github.com>
2025-03-01 14:21:51 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -146,10 +146,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
